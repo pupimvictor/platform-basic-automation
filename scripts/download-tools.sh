23061b8c3ed49116b793cc9a065921d599e7edfc
victorpupim <pupimvictor@gmail.com>
2020-07-21 07:16:23 -0700

    add new tools to downlaod_tools

--- scripts/download-tools.sh
+++ scripts/download-tools.sh
@@ -52,3 +52,41 @@ function download_pivnet_cli () {
 
     download_and_install $release_location /usr/local/bin/pivnet
 }
+
+function download_helm () {
+    local version="${1:-v3.3.0}"
+    local arch="${2:-linux-amd64}"
+
+    local bin_name="helm-$version-$arch.tar.gz"
+
+    local release_url="https://get.helm.sh/$bin_name"
+
+    wget "$release_url" -O $bin_name
+    if [[ $? != 0 ]]; then
+        printErr "could find helm release at $release_url"
+        return 1
+    fi
+    
+    tar -C . -xzvf $bin_name
+    if [[ $? != 0 ]]; then
+        printErr "failed decrompressing $bin_name"
+        return 1
+    fi
+    
+    chmod_and_mv $arch/helm /usr/local/bin/helm
+
+    rm -rf $bin_name
+    rm -rf $arch
+}
+
+function download_pks_cli () {
+    [[ -f "${__DIR}/login-pivnet.sh" ]] &&  \
+        source "${__DIR}/login-pivnet.sh" ||  \
+        echo "login-pivnet.sh not found"
+
+    pivnet download-product-files --product-slug='pivotal-container-service' --release-version='1.8.1' --product-file-id=737302
+    
+    local bin_name="pks-linux-amd64-1.8.0-build.75"
+    
+    chmod_and_mv $__DIR/$bin_name /usr/local/bin/pks
+}
